0492b3d3ec65895aa10dd4aa09964eb690cc8b01
Erick Nava <erickbtns@gmail.com>
2025-02-21 16:40:40 -0600

    Segunda parte de la practica. El archivo pedira el nombre del usuario

--- practicas/3/NavaErick/Programa.cpp
+++ practicas/3/NavaErick/Programa.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main(){
-  cout << "Soy Erick y este es un programa en c++" << endl;
+  string a;  //tipo de variable
+  cout <<"Introducir nombre para presentarte" << endl; //solicitud del dato
+  cin >> a; //lectura del nombre
+  cout << "Soy " << a <<" y este es un programa en c++" << endl; //Se imprime la frase
   return 0;
 }
