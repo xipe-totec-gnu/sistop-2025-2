874d5777b355f889ff34177f1d593d47f10aea18
Eric Ramirez <eric.ram.val@gmail.com>
2025-02-24 12:22:58 -0600

    Agrego modificación del primer archivo para que imprima la hora actual añadeindo la hora actual al pulsar en el botón Print Time

--- practicas/3/RamirezEric/primer.py
+++ practicas/3/RamirezEric/primer.py
@@ -2,13 +2,15 @@ import tkinter as tk
 from datetime import datetime
 
 def print_time():
+    nombre = input("¿Cuál es tu nombre? ")
     current_time = datetime.now().strftime("%H:%M:%S")
-    print("Current Time:", current_time)
+    print(nombre," la hora es:", current_time)
+    
 
 root = tk.Tk()
 root.title("Print Time Button")
 
 print_time_button = tk.Button(root, text="Print Time", command=print_time)
-print_time_button.pack(pady=20)
+print_time_button.pack(pady=100)
 
 root.mainloop()
