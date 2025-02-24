0dbea1c064e14fcbebcffd429ce7ecb27a5784de
Urbano Meza Joseph Gael <urbanojosephgael@gmail.com>
2025-02-24 13:08:53 -0600

    Agrego la modificacion del programa

--- practicas/3/programa1.py
+++ practicas/3/programa1.py
@@ -1,3 +1,11 @@
-#Este es un programa que manda un saludo#
+#Este es un programa que obtiene el nombre de un usuario desde la linea de comando"
 
-print("Hola Mundo")
+def main():
+    # Solicita al usuario que ingrese su nombre
+    nombre = input("Por favor, ingresa tu nombre: ")
+    
+    # Muestra un saludo personalizado
+    print(f"¡Hola, {nombre}! Bienvenido/a.")
+
+if __name__ == "__main__":
+    main()
