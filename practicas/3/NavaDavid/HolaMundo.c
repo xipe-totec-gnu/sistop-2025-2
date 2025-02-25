97a138e911756c77c7d06487742981e406128f5f
davnv29 <emilionave29@gmail.com>
2025-02-24 19:06:57 -0600

    Modificacion Hola Mundo P3

--- practicas/3/NavaDavid/HolaMundo.c
+++ practicas/3/NavaDavid/HolaMundo.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+#define LONGITUD_MAXIMA 50
 int main(){
-    printf("Nava David - Práctica 3");
-    printf("Hola mundo");
+    char nombre[LONGITUD_MAXIMA];
+    printf("Nava David - Práctica 3 Segunda Parte\n");
+    printf("Ingresa tu nombre: ");
+    scanf("%s", nombre);
+    printf("Hola, %s",nombre);
+    getch();
     return 0;
 }
