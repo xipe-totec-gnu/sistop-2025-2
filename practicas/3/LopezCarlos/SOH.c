aa06299cc7457286a37325ea3e59c19f3f98f008
carloscii203@gmail.com <carloscii203@gmail.com>
2025-02-24 21:40:24 -0600

    Acabo de modificar algo en el doc jeje

--- practicas/3/LopezCarlos/SOH.c
+++ practicas/3/LopezCarlos/SOH.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    int a, b;
+    int a, b, c;
     printf("Ingresa un numero: ");
     scanf("%d", &a);
+    c = a * 2;
+    printf("el resultado es: %i", &c);
 }
