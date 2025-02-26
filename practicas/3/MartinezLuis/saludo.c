8ea48f6d5a6ccb37a32f59ea7b249e7b4563fcd0
Luis Angel Martinez Garcia <martinez.luis.angel.1000@gmail.com>
2025-02-26 16:24:01 -0600

    El programa ahora lee nombres

--- practicas/3/MartinezLuis/saludo.c
+++ practicas/3/MartinezLuis/saludo.c
@@ -2,7 +2,13 @@
 
 int main()
 {
-    printf("Hello World");
+    char nombre[100];
+    printf("Ingresa tu nombre\n");  
+    if (fgets(nombre, sizeof(nombre), stdin) != NULL) { 
+        printf("Hola %s¿Cómo estas?\n", nombre); 
+    } else { 
+        printf("\n"); 
+    }
 
     return 0;
 }
