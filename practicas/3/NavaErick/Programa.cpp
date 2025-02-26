c5df39a624bc32a32c97b4ef37d2b8fdfbace858
Erick Nava <erickbtns@gmail.com>
2025-02-25 22:41:06 -0600

    Agrego el programa

--- practicas/3/NavaErick/Programa.cpp
+++ practicas/3/NavaErick/Programa.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 using namespace std;
-int main(){
-  string a;  //tipo de variable
-  cout <<"Introducir nombre para presentarte" << endl; //solicitud del dato
-  cin >> a; //lectura del nombre
-  cout << "Soy " << a <<" y este es un programa en c++" << endl; //Se imprime la frase
+int main (){
+  cout << "Hola Usuario, ejecutaste el programa de Erick :D" << endl;
+
+
   return 0;
 }
