ce38414cf818a2b4b40a55c4a5971f0aef886e0c
Erick Nava <erickbtns@gmail.com>
2025-02-25 22:50:22 -0600

    Se hicieron modificaciones en el programa para la practica 3

--- practicas/3/NavaErick/Programa.cpp
+++ practicas/3/NavaErick/Programa.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 int main (){
-  cout << "Hola Usuario, ejecutaste el programa de Erick :D" << endl;
-
+  string a;
+  cout <<"Introduce tu nombre" << endl;
+  cin >> a;
+  cout << "Hola " << a << ", ejecutaste el programa de Erick :D" << endl;
+  //Cambios realizados:
+  // se solicita el nombre del usuario para posteriormente iprimirlo en pantalla
 
   return 0;
 }
