ce4fcb0ce9b45f11ccbb9210f5967423c37f38f2
MelendezAnuar <anuarmxn@hotmail.com>
2025-02-25 22:46:32 -0600

    Se agrega archivo en java Que imprime el mensaje Hola soooooy Anuar

---
+++ practicas/3/MelendezAnuar/HolaAnuar.java
@@ -0,0 +1,7 @@
+// Nombre del archivo: HolaAnuar.java
+
+public class HolaAnuar {
+    public static void main(String[] args) {
+        // Imprime "Hola sooooooy Anuar" en la consola
+        System.out.println("Hola soooooy Anuar");
+    }A
