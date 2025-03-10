06f281df1450b3382d4e31d79eb1af9619c9c1b3
ksobrenat32 <ksobrenatural@tuta.io>
2025-03-10 08:05:33 -0600

    P4: agregar fibonacci y conteo de tiempo en el programa de hilos

--- practicas/4/CalderonEnrique/threads.c
+++ practicas/4/CalderonEnrique/threads.c
@@ -4,11 +4,28 @@
 #include <unistd.h>
 #include <time.h>
 
+typedef struct {
+    unsigned long long fib_n;
+    time_t init_time;
+} thread_args;
+
+// Recursive fibonacci function
+unsigned long long fib(unsigned long long n) {
+    if (n == 0) return 1;
+    if (n == 1) return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
 void *thread_function(void *arg) {
-    sleep(1);
-    int pid = getpid();
-    pthread_t tid = pthread_self();
-    printf("Hello from pid %d and tid %lu at %ld\n", pid, tid, time(NULL));
+    thread_args *targ = (thread_args *) arg;
+
+    time_t init_time = time(NULL) - targ->init_time;
+
+    unsigned long long n = targ->fib_n;
+    unsigned long long res = fib(n);
+
+    printf("Thread #%ld started at %ld, finished at %ld, the result is %llu\n", n, init_time, time(NULL) - targ->init_time, res);
+    return NULL;
 }
 
 int main(int argc, char *argv[]) {
@@ -19,17 +36,20 @@ int main(int argc, char *argv[]) {
 
     int thread_num = atoi(argv[1]);
 
-    printf("Initial time: %ld\n", time(NULL));
+    time_t init_time = time(NULL);
 
     pthread_t threads[thread_num];
-    for (int i = 0; i < thread_num; i++) {
-        pthread_create(&threads[i], NULL, thread_function, NULL);
+    for (unsigned long long i = 0; i < thread_num; i++) {
+        thread_args *arg = (thread_args *) malloc(sizeof(thread_args));
+        arg->fib_n = i;
+        arg->init_time = init_time;
+        pthread_create(&threads[i], NULL, thread_function, arg);
     }
 
     for (int i = 0; i < thread_num; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    printf("Final time: %ld\n", time(NULL));
+    printf("Final time: %ld\n", time(NULL) - init_time);
     return 0;
 }
