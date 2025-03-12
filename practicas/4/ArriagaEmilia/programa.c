52dd50a3c0cd73033a85b687f09bfb90edfa8f47
Emilia Arriaga <emiliaarriaga002@gmail.com>
2025-03-11 18:30:09 -0600

    segundo commit

--- practicas/4/ArriagaEmilia/programa.c
+++ practicas/4/ArriagaEmilia/programa.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
-#include <omp.h>
 
 #define N 1000
 
 int main() {
     int array[N];
     int sum = 0;
-    
+
     // Inicializar el arreglo
     for (int i = 0; i < N; i++) {
         array[i] = i + 1;
     }
-    
-    // Calcular la suma en paralelo
-    #pragma omp parallel for reduction(+:sum)
+
+    // Calcular la suma de manera secuencial
     for (int i = 0; i < N; i++) {
         sum += array[i];
     }
-    
+
     printf("Suma total: %d\n", sum);
     return 0;
 }
+
