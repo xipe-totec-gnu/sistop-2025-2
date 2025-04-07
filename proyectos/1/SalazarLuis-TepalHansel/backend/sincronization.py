1526ce9b0c045926f0a7d0514388b52efc94c455
Luis Salazar <salazarislas.daniel@gmail.com>
2025-04-07 07:49:51 -0600

    Diseño de los sprites y programa básico en terminal

--- proyectos/1/SalazarLuis-TepalHansel/backend/sincronization.py
+++ proyectos/1/SalazarLuis-TepalHansel/backend/sincronization.py
@@ -1,26 +1,128 @@
 import threading
 import time
-
+import tkinter as tk
+import random
+import queue
 numWorkers=3
-mutexTrabajador=threading.Semaphore(0)
+contador=0
+mutexContador=threading.Semaphore(1)
+TrabajadorSleep=[threading.Semaphore(0) for _ in range(numWorkers)]
+TrabajadorActivo=[threading.Semaphore(0) for _ in range(numWorkers)]
+mutexAtendiendo=[threading.Semaphore(1) for _ in range(numWorkers)]
+mutexAvailable=threading.Semaphore(1)
+available=queue.Queue()
 
-def workers(id):
-    while True:
-        mutexTrabajador.acquire()
-        print("Soy el trabajador f{id} y no tengo nada que hacer")
+
+for i in range(numWorkers):
+    available.put(i)
+
+capacityAlumnos=threading.Semaphore(numWorkers)
+verticalDes=10
+
+class AlumnoSprite:
+    contador=0
+    def __init__(self,canvas,leftUpperCorner,rightBottomCorner):
+        self.contador+=1
+        self.setImage(canvas,leftUpperCorner,rightBottomCorner)
+        self.colors=["red","yellow","blue","green","orange","purple"]
+
+    def setImage(self,canvas,leftUpperCorner,rightBottomCorner):
+        self.canvas=canvas
+        self.leftUpperCorner=leftUpperCorner
+        self.rightBottomCorner=rightBottomCorner
+        self.width,self.height=tuple(rightBottomCorner[i]-leftUpperCorner[i] for i in range(len(leftUpperCorner)))
+        self.widthHead=self.width
+        self.heightHead=self.height/2
+        self.circleLeftUpperCorner=self.leftUpperCorner
+        self.circleRightBottomCorner=(self.leftUpperCorner[0]+self.widthHead,self.leftUpperCorner[1]+self.heightHead)
+        self.widthBody=self.width
+        self.heightBody=self.height-self.heightHead
+        self.bodyLeftUpperCorner=(self.rightBottomCorner[0]-self.widthBody,self.rightBottomCorner[1]-self.heightBody)
+        self.bodyRightBottomCorner=self.rightBottomCorner
+
+    def build(self):
+        self.canvas.create_oval(
+            *self.circleLeftUpperCorner,
+            *self.circleRightBottomCorner,
+            fill="pink",
+            outline="black",
+            width=2
+        )
+        self.canvas.create_rectangle(
+            *self.bodyLeftUpperCorner,
+            *self.bodyRightBottomCorner,
+            fill=self.colors[random.randint(0,len(self.colors)-1)],
+            outline="black",
+            width=2
+        )
+        centerX=(self.bodyLeftUpperCorner[0]+self.bodyRightBottomCorner[0])/2
+        centerY=(self.bodyLeftUpperCorner[1]+self.bodyRightBottomCorner[1])/2
+        self.canvas.create_text(
+            centerX,centerY,
+            text=f"{self.contador}",
+            font=("Arial",10,"bold")
+        )
         
-def alumnos(id):
-    print("Don Rata atiendame")
+
+def workers(id,worker=None,canvas=None):
+    while True:
+        if __name__=="__main__":
+            print(f"Soy el trabajador {id} y no tengo nada que hacer")
+            TrabajadorSleep[id].acquire()
+            print(f"Atendiendo {id}")
+            TrabajadorActivo[id].acquire()
 
 
-#Se crean los trabajadores
-def donRata():
+        else:
+            canvas.itemconfig(worker,fill="white")
+            #TrabajadorSleep[id].acquire()
+            canvas.itemconfig(worker,fill="red")
+        
+def alumnos(workers=None,canvas=None):
+    global verticalDes
+    global available
+    global contador
+    with mutexContador:
+        contador+=1
+        idAlumno=contador
+    
+    if __name__=="__main__":
+        idWorker=available.get(block=True,timeout=None)
+        print(f"Es el turno de {idAlumno}, pasa a {idWorker}")
+
+        #Pasa por algunas fases
+
+        TrabajadorSleep[idWorker].release()
+        print(f"{idAlumno} recibio su pedido")
+        TrabajadorActivo[idWorker].release()
+        print(f"Se libera {idWorker}")
+        available.put(idWorker)
+
+    else:
+            #capacityAlumnos.acquire()
+        with mutexAvailable:
+            #idWorker=available.pop()
+            idWorker=2
+        with mutexAtendiendo[idWorker]:
+            coords=canvas.coords(workers[idWorker])
+            coords[1]+=verticalDes
+            alumno=AlumnoSprite((coords[0],coords[1]),(coords[0]+20,coords[1]+20))
+            alumno.build()
+            print("Don Rata atiendame")
+            with mutexAvailable:
+                available.append(idWorker)
+            #TrabajadorSleep[idWorker].release()
+
+if __name__=="__main__":
     for i in range(numWorkers):
-        threading.Thread(target=workers, args=[i]).start()
-    cont = 0
-    while(True):
-        cont +=1
-        threading.Thread(target=alumnos, args=[cont]).start()
+        threading.Thread(target=workers,args=[i]).start()
+    while True:
+        threading.Thread(target=alumnos,args=[]).start()
+    
+
+
+    
+
         
 
 
--- proyectos/1/SalazarLuis-TepalHansel/frontend/interfaz.py
+++ proyectos/1/SalazarLuis-TepalHansel/frontend/interfaz.py
@@ -4,59 +4,14 @@ import sys
 import os
 # Add the parent directory of the 'backend' folder to the system path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import backend.sincronization as sinc
+import queue 
+import backend.sincronization as bs
 import threading
-import queue
 import random
 import time
 
-class alumnoSprite:
-    contador=0
-    def __init__(self,canvas,leftUpperCorner,rightBottomCorner):
-        self.contador+=1
-        self.setImage(canvas,leftUpperCorner,rightBottomCorner)
-        self.colors=["red","yellow","blue","green","orange","purple"]
 
-    def setImage(self,canvas,leftUpperCorner,rightBottomCorner):
-        self.canvas=canvas
-        self.leftUpperCorner=leftUpperCorner
-        self.rightBottomCorner=rightBottomCorner
-        self.width,self.height=tuple(rightBottomCorner[i]-leftUpperCorner[i] for i in range(len(leftUpperCorner)))
-        self.widthHead=self.width
-        self.heightHead=self.height/2
-        self.circleLeftUpperCorner=self.leftUpperCorner
-        self.circleRightBottomCorner=(self.leftUpperCorner[0]+self.widthHead,self.leftUpperCorner[1]+self.heightHead)
-        self.widthBody=self.width
-        self.heightBody=self.height-self.heightHead
-        self.bodyLeftUpperCorner=(self.rightBottomCorner[0]-self.widthBody,self.rightBottomCorner[1]-self.heightBody)
-        self.bodyRightBottomCorner=self.rightBottomCorner
-
-    def build(self):
-        self.canvas.create_oval(
-            *self.circleLeftUpperCorner,
-            *self.circleRightBottomCorner,
-            fill="pink",
-            outline="black",
-            width=2
-        )
-        self.canvas.create_rectangle(
-            *self.bodyLeftUpperCorner,
-            *self.bodyRightBottomCorner,
-            fill=self.colors[random.randint(0,len(self.colors)-1)],
-            outline="black",
-            width=2
-        )
-        centerX=(self.bodyLeftUpperCorner[0]+self.bodyRightBottomCorner[0])/2
-        centerY=(self.bodyLeftUpperCorner[1]+self.bodyRightBottomCorner[1])/2
-        self.canvas.create_text(
-            centerX,centerY,
-            text=f"{self.contador}",
-            font=("Arial",10,"bold")
-        )
-        
-
-class tiendaSprite:
+class TiendaSprite:
     def __init__(self,canvas,leftUpperCorner,rightBottomCorner,base,diameter,dFromUpper):
         self.canvas=canvas
         self.leftUpperCorner=leftUpperCorner
@@ -69,7 +24,7 @@ class tiendaSprite:
         self.dFromUpper=dFromUpper
 
     def build(self):
-        
+        workers=[]
         self.canvas.create_rectangle(
                 self.leftUpperCorner[0], self.leftUpperCorner[1],  # Esquina rightUpperCorner izquierda (x1, y1)
                 self.rightBottomCorner[0], self.rightBottomCorner[1], # Esquina leftUpperCorner derecha (x2, y2)
@@ -77,35 +32,38 @@ class tiendaSprite:
                 outline="black",   # Color del borde
                 width=2           # Grosor del borde
             )
-        for i in range(sinc.numWorkers):
+        for i in range(bs.numWorkers):
             newX=self.leftUpperCorner[0]+self.base+self.desplazamiento*i-self.diameter/2
             newY=self.leftUpperCorner[1]+self.dFromUpper
+            workers.append(
             self.canvas.create_oval(
                 newX,newY,
                 newX+self.diameter,newY+self.diameter,
                 fill="white",  # Color de relleno
                 outline="black",   # Color del borde
                 width=2           # Grosor del borde
-            )
+            ))
+        return workers
+
 
 class TiendaApp:
     def __init__(self, root):
         self.root = root
-        self.lights=[]
+        self.workers=[]
+        self.canvas = tk.Canvas(root, width=400, height=300, bg="white")
+        self.canvas.pack()
         #Título de la interfaz
         self.root.title("Simulación de Tienda - Pedidos Concurrentes")
         # Interfaz gráfica
         self.setup_ui()
         # Inicio de los trabajadores
+        self.start()
 
-        
-
-    def log(self, mensaje):
-        """Muestra un mensaje en el área de logs."""
-        self.log_area.config(state="normal")
-        self.log_area.insert(tk.END, mensaje + "\n")
-        self.log_area.config(state="disabled")
-        self.log_area.see(tk.END)  # Auto-scroll
+    def start(self):
+        for i in range(bs.numWorkers):
+            threading.Thread(target=bs.workers, args=[self.canvas,i,self.workers[i],self.root],daemon=True).start()
+        for i in range(10):
+            threading.Thread(target=bs.alumnos, args=[self.canvas,self.workers]).start()
     
     def setup_ui(self):
         # Frame principal
@@ -114,22 +72,12 @@ class TiendaApp:
         #frame.pack(expand=True)
         # Título
         tk.Label(frame, text="🏪 Simulación de Don Rata", font=("Arial", 16)).pack(pady=5)
-        
-        canvas = tk.Canvas(root, width=400, height=300, bg="white")
-        canvas.pack()
 
-        tienda=tiendaSprite(canvas=canvas,leftUpperCorner=(50,50),rightBottomCorner=(350,100),base=30,diameter=20,dFromUpper=20)
-        tienda.build()
+        tienda=TiendaSprite(canvas=self.canvas,leftUpperCorner=(50,50),rightBottomCorner=(350,100),base=30,diameter=20,dFromUpper=20)
+        self.workers=tienda.build()
 
-        alumno=alumnoSprite(canvas=canvas,leftUpperCorner=(50,200),rightBottomCorner=(80,230))
-        alumno.build()
-                
-        # Área de logs (para mostrar eventos)
-        
-        #self.log_area = scrolledtext.ScrolledText(frame, width=60, height=20, state="disabled")
-        #self.log_area.pack(pady=10)
-        
-        # Botón para agregar cliente manualmente
+        #alumno=alumnoSprite(canvas=canvas,leftUpperCorner=(50,200),rightBottomCorner=(80,230))
+        #alumno.build()
         
         # Botón para detener la simulación
         tk.Button(frame, text="Detener Simulación", command=self.detener_simulacion).pack(pady=5)
