76adea2a2fb8563df49d034a64d1e5ed854b7ba8
EldoSantos2004 <leomikel0515@gmail.com>
2025-04-12 11:18:00 -0600

    Agregar funcionalidades
    Agregar funcionalidad de hacer preguntas y de imprimir material

--- proyectos/1/CervantesLeonardo-SoleArnau/competencia.py
+++ proyectos/1/CervantesLeonardo-SoleArnau/competencia.py
@@ -22,13 +22,16 @@ from collections import deque
 
 
 # El numero de equipos participantes en la competencia de programacion
-number_of_participants = 2
+number_of_participants = 3
 
 # El numero de validadores de soluciones del juez
 number_of_validators = 3 
 
 # El numero de problemas del concurso 
 number_of_problems = 9
+
+# El numero de impresoras
+number_of_printers = 2
 
 # Cola de problemas a ser juzgados
 problem_queue = deque()
@@ -39,12 +42,17 @@ queue_size = threading.Semaphore(0)
 # Mutex para evitar que varios hilos modifiquen a la cola de manera simultanea
 mutex_queue = threading.Semaphore(1)
 
+# Barrera para que los participantes esperen a que se junten 3 archivos antes de imprimir
+barrier_printers = threading.Barrier(number_of_printers)
+
+# Mutex para responder clarificaciones
+mutex_clarification = threading.Semaphore(1)
 
 # Lista de eventos para que participantes esperen a que solucion sea juzgada antes de volver a subir
-waiting_for_solution = [threading.Event(), threading.Event(), threading.Event()]
+waiting_for_solution = [threading.Event() for _ in range(number_of_participants+1)]
 
 # Bandera para observar que problemas fueron aceptados
-accepted = [0, 0, 0]
+accepted = [0] * (number_of_participants+1)
 
 
 
@@ -59,6 +67,14 @@ PROBLEM_COLOR_EMOJIS = PROBLEM_COLOR_EMOJIS[:number_of_problems]
 # Posibles veredictos a solucion de un problema
 possible_veredicts = ['AC (Accepted)', 'WA (Wrong Answer)', 'TLE (Time Limit Excedeed)', 'RTE (Runtime Error)', 'Presentation Error']
 
+def ask_to_print(problem_num):
+  # aqui se va a recibir el numero de pregunta de problema pero se van a responder hasta que se junten 3 preguntas
+  # Se espera a que se junten 3 preguntas
+  barrier_printers.wait()
+  # Se responde la pregunta
+  print(f"Question {problem_num} has been answered")
+  # Se reinicia la barrera
+  barrier_printers.reset()
 
 
 def participant(participant_num, problems_not_solved):
@@ -67,6 +83,26 @@ def participant(participant_num, problems_not_solved):
     time.sleep(random.uniform(0, 1))
     # El equipo escoge uno de los problemas que no ha resuelto e intenta subir una solucion a este
     problem_to_submit = random.choice(problems_not_solved)
+    # De manera aleatoria se decide si se va a a mandar una pregunta de aclaracion
+    if random.choice([True, False]):
+      print(f"Participant {participant_num} has a question about problem {problem_to_submit}")
+      # Se manda una pregunta de aclaracion
+      mutex_clarification.acquire()
+      # Se duerme un poco para simular el tiempo que tarda en responder la pregunta
+      time.sleep(random.uniform(0, 1))
+      # Se responde la pregunta
+      print(f"Participant {participant_num} has received answer to question about problem {problem_to_submit}")
+      mutex_clarification.release()
+    # De manera aleatoria se decide si se va a a mandar a imprimir problema
+    if random.choice([True, False]):
+      print(f"Participant {participant_num} wants to print code for problem {problem_to_submit}")
+      # Se manda a imprimir un archivo
+      ask_to_print(problem_to_submit)
+      # Cuando se sale de la funcion ya se respondio la pregunta
+      print(f"Participant {participant_num} has received printed file about problem {problem_to_submit}")
+    # Se duerme un poco para simular el tiempo que tarda en subir la solucion
+    time.sleep(random.uniform(0, 1))
+    # Se sube la solucion al problema
     print(f"Participant number {participant_num} has submitted a solution to {problem_to_submit}")
     mutex_queue.acquire()
     # Se inserta la solucion a la cola de problemas a ser juzgados
